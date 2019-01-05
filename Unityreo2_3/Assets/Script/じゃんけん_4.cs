efeb71c54900cbbe0f2d2375659a41b551e95c90
darkreo666 <holycat666@gmail.com>
2019-01-05 20:33:45 +0900

    習作ゲーム

---
+++ Unityreo2_3/Assets/Script/じゃんけん_4.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class じゃんけん_4 : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            SceneManager.LoadScene("タイトル");
+        }
+    }
+}
---
+++ Unityreo2_3/Assets/Script/タイトル.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class タイトル : MonoBehaviour
+{
+    public bool canMoveNext; // falseにしておく
+    public float startDelaytimes;
+    // 画面繊維可能メッセージ
+    GameObject nextText;
+    public bool waitflag; // 普通はfalseにしておく
+
+    // 主人公のライフ
+    public int life;
+
+    // 敵のライフ
+    public int enemyLife;
+
+    // Use this for initialization
+    public void Start()
+    {
+        nextText = GameObject.Find("nextText");
+        nextText.SetActive(false);
+        Invoke("startDelay", startDelaytimes);
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        gameMain();
+        if (Input.GetKeyUp(KeyCode.Space) && waitflag == true && canMoveNext == true)
+        {
+            SceneManager.LoadScene("じゃんけん_1");
+        }
+    }
+    void startDelay()
+    {
+        waitflag = true;
+    }
+    public void actJanken()
+    {
+
+    }
+    public void gameMain()
+    {
+        actJanken();
+        // ゲームオーバー
+        if (life <= 0)
+        {
+            // ゲームオーバー処理
+            SceneManager.LoadScene("ゲームオーバー");
+        }
+
+        // 勝利！
+        if (enemyLife <= 0)
+        {
+            // 画面遷移可能処理
+            nextText.SetActive(true);
+            canMoveNext = true;
+        }
+    }
+}
